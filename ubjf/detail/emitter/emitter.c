7cedf5f6eaf1c66f550df0df0b82d78152e81649
SwitchBlade <justclaireee@yahoo.com>
2022-03-03 22:45:44 -0800

    Removed version-specific ubjf_write_array implementation since it is not needed.

--- ubjf/detail/emitter/emitter.c
+++ ubjf/detail/emitter/emitter.c
@@ -10,7 +10,6 @@ typedef struct
 	ubjf_error (*const start_container)(ubjf_write_state *, ubjf_container_info);
 	ubjf_error (*const write_object_key)(ubjf_write_state *, ubjf_string);
 	ubjf_error (*const end_container)(ubjf_write_state *);
-	ubjf_error (*const write_array)(ubjf_write_state *restrict, const ubjf_value *restrict, int64_t, ubjf_type);
 } ubjf_emitter_vtable;
 
 #ifndef UBJF_NO_SPEC_12
@@ -18,7 +17,6 @@ ubjf_error ubjf_s12_write_value(ubjf_write_state *, ubjf_value);
 ubjf_error ubjf_s12_start_container(ubjf_write_state *, ubjf_container_info);
 ubjf_error ubjf_s12_write_object_key(ubjf_write_state *, ubjf_string);
 ubjf_error ubjf_s12_end_container(ubjf_write_state *);
-ubjf_error ubjf_s12_write_array(ubjf_write_state *restrict, const ubjf_value *restrict, int64_t, ubjf_type);
 #endif
 
 static const ubjf_emitter_vtable ubjf_emitter_syntax_table[] = {
@@ -28,7 +26,6 @@ static const ubjf_emitter_vtable ubjf_emitter_syntax_table[] = {
 				.start_container = ubjf_s12_start_container,
 				.write_object_key = ubjf_s12_write_object_key,
 				.end_container = ubjf_s12_end_container,
-				.write_array = ubjf_s12_write_array,
 		},
 #endif
 };
@@ -49,7 +46,34 @@ ubjf_error ubjf_end_container(ubjf_write_state *state)
 {
 	return ubjf_emitter_syntax_table[state->syntax].end_container(state);
 }
+
 ubjf_error ubjf_write_array(ubjf_write_state *state, const ubjf_value *data, int64_t n, ubjf_type value_type)
 {
-	return ubjf_emitter_syntax_table[state->syntax].write_array(state, data, n, value_type);
+#ifndef UBJF_DISABLE_CHECKS
+	if (UBJF_UNLIKELY(!state))
+		return UBJF_MAKE_PARAM_ERROR(0);
+	else if (UBJF_UNLIKELY(!data))
+		return UBJF_MAKE_PARAM_ERROR(1);
+	else if (UBJF_UNLIKELY(n < 0))
+		return UBJF_MAKE_PARAM_ERROR(2);
+#endif
+
+	ubjf_container_info info = {
+			.container_type = UBJF_ARRAY,
+			.length = n,
+			.value_type = value_type,
+	};
+
+	ubjf_error result = ubjf_start_container(state, info);
+	if (UBJF_UNLIKELY(result != UBJF_NO_ERROR))
+		return result;
+
+	for (int64_t i = 0; i < n; ++i)
+	{
+		result = ubjf_write_value(state, data[i]);
+		if (UBJF_UNLIKELY(result != UBJF_NO_ERROR))
+			return result;
+	}
+
+	return ubjf_end_container(state);
 }
--- ubjf/detail/emitter/emitter_spec12.c
+++ ubjf/detail/emitter/emitter_spec12.c
@@ -286,37 +286,4 @@ ubjf_error ubjf_s12_end_container(ubjf_write_state *state)
 	ubjf_s12_pop_info(state);
 	return UBJF_NO_ERROR;
 }
-
-ubjf_error ubjf_s12_write_array(ubjf_write_state *restrict state, const ubjf_value *restrict data, int64_t n,
-                                ubjf_type value_type)
-{
-#ifndef UBJF_DISABLE_CHECKS
-	if (UBJF_UNLIKELY(!state))
-		return UBJF_MAKE_PARAM_ERROR(0);
-	else if (UBJF_UNLIKELY(!data))
-		return UBJF_MAKE_PARAM_ERROR(1);
-	else if (UBJF_UNLIKELY(n < 0))
-		return UBJF_MAKE_PARAM_ERROR(2);
-#endif
-
-	ubjf_container_info info = {
-			.container_type = UBJF_ARRAY,
-			.length = n,
-			.value_type = value_type,
-	};
-
-	ubjf_error result = ubjf_s12_start_container(state, info);
-	if (UBJF_UNLIKELY(result != UBJF_NO_ERROR))
-		return result;
-
-	for (int64_t i = 0; i < n; ++i)
-	{
-		result = ubjf_s12_write_value(state, data[i]);
-		if (UBJF_UNLIKELY(result != UBJF_NO_ERROR))
-			return result;
-	}
-
-	/* If got here, the only error can be returned by `ubjf_s12_end_container`. */
-	return ubjf_s12_end_container(state);
-}
 #endif
